3bd9ff0ba9e17ab5de56a1033f52de0bcbd0d4af
J.A. Roberts Tunney <jtunney@lobstertech.com>
2009-08-07 14:43:27 -0400

    ok let's get rid of all that profiling stuff i don't understand. kmemcached is really cool so there's probably more value in simplifying the profiling server so people might actually stand a chance of getting it to work.

--- django_extensions/management/commands/runprofileserver.py
+++ django_extensions/management/commands/runprofileserver.py
@@ -1,12 +1,28 @@
 """
 runprofileserver.py
 
-    Starts a lightweight Web server with profiling enabled.
+Starts a lightweight Web server with profiling enabled.  Produces output
+for CacheGrind.
 
-Credits for kcachegrind support taken from lsprofcalltree.py go to:
- David Allouche
- Jp Calderone & Itamar Shtull-Trauring
- Johan Dahlin
+Debian/Ubuntu Users:
+
+sudo apt-get install kcachegrind kcachegrind-converters
+
+To Use:
+
+$ ./manage.py --nomedia --prof-path=profiles
+
+*go crazy visiting pages like /blog/love/*
+
+$ ls profiles
+blog.love_000050ms_2009-08-07T13:28:56.649857.prof
+blog.love_000002ms_2009-08-07T13:29:00.643459.prof
+
+Why did the first request take 50 milliseconds?  :(
+
+$ kcachegrind blog.love_000050ms_2009-08-07T13:28:56.649857.prof
+
+The rest is totally amazing.
 """
 
 from django.core.management.base import BaseCommand, CommandError
@@ -14,6 +30,8 @@ from optparse import make_option
 from datetime import datetime
 import os
 import sys
+import time
+import thread
 
 def label(code):
     if isinstance(code, str):
@@ -22,73 +40,6 @@ def label(code):
         return '%s %s:%d' % (code.co_name,
                              code.co_filename,
                              code.co_firstlineno)
-
-class KCacheGrind(object):
-    def __init__(self, profiler):
-        self.data = profiler.getstats()
-        self.out_file = None
-
-    def output(self, out_file):
-        self.out_file = out_file
-        print >> out_file, 'events: Ticks'
-        self._print_summary()
-        for entry in self.data:
-            self._entry(entry)
-
-    def _print_summary(self):
-        max_cost = 0
-        for entry in self.data:
-            totaltime = int(entry.totaltime * 1000)
-            max_cost = max(max_cost, totaltime)
-        print >> self.out_file, 'summary: %d' % (max_cost,)
-
-    def _entry(self, entry):
-        out_file = self.out_file
-
-        code = entry.code
-        #print >> out_file, 'ob=%s' % (code.co_filename,)
-        if isinstance(code, str):
-            print >> out_file, 'fi=~'
-        else:
-            print >> out_file, 'fi=%s' % (code.co_filename,)
-        print >> out_file, 'fn=%s' % (label(code),)
-
-        inlinetime = int(entry.inlinetime * 1000)
-        if isinstance(code, str):
-            print >> out_file, '0 ', inlinetime
-        else:
-            print >> out_file, '%d %d' % (code.co_firstlineno, inlinetime)
-
-        # recursive calls are counted in entry.calls
-        if entry.calls:
-            calls = entry.calls
-        else:
-            calls = []
-
-        if isinstance(code, str):
-            lineno = 0
-        else:
-            lineno = code.co_firstlineno
-
-        for subentry in calls:
-            self._subentry(lineno, subentry)
-        print >> out_file
-
-    def _subentry(self, lineno, subentry):
-        out_file = self.out_file
-        code = subentry.code
-        #print >> out_file, 'cob=%s' % (code.co_filename,)
-        print >> out_file, 'cfn=%s' % (label(code),)
-        if isinstance(code, str):
-            print >> out_file, 'cfi=~'
-            print >> out_file, 'calls=%d 0' % (subentry.callcount,)
-        else:
-            print >> out_file, 'cfi=%s' % (code.co_filename,)
-            print >> out_file, 'calls=%d %d' % (
-                subentry.callcount, code.co_firstlineno)
-
-        totaltime = int(subentry.totaltime * 1000)
-        print >> out_file, '%d %d' % (lineno, totaltime)
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
@@ -100,10 +51,6 @@ class Command(BaseCommand):
             help='Specifies the directory which to save profile information in.'),
         make_option('--nomedia', action='store_true', dest='no_media', default=False,
             help='Do not profile MEDIA_URL and ADMIN_MEDIA_URL'),
-        make_option('--use-cprofile', action='store_true', dest='use_cprofile', default=False,
-            help='Use cProfile if available, this is disabled per default because of incompatibilities.'),
-        make_option('--kcachegrind', action='store_true', dest='use_lsprof', default=False,
-            help='Create kcachegrind compatible lsprof files, this requires and automatically enables cProfile.  Debian: apt-get install kcachegrind kcachegrind-converters'),
     )
     help = "Starts a lightweight Web server with profiling enabled."
     args = '[optional port number, or ipaddr:port]'
@@ -141,19 +88,6 @@ class Command(BaseCommand):
             from django.conf import settings
 
             import hotshot, time, os
-            USE_CPROFILE = options.get('use_cprofile', False)
-            USE_LSPROF = options.get('use_lsprof', False)
-            if USE_LSPROF:
-               USE_CPROFILE = True
-            if USE_CPROFILE:
-                try:
-                    import cProfile
-                    USE_CPROFILE = True
-                except ImportError:
-                    print "cProfile disabled, module cannot be imported!"
-                    USE_CPROFILE = False
-            if USE_LSPROF and not USE_CPROFILE:
-                raise SystemExit("Kcachegrind compatible output format required cProfile from Python 2.5")
             prof_path = options.get('prof_path', '/tmp')
             def make_profiler_handler(inner_handler):
                 def handler(environ, start_response):
@@ -166,10 +100,7 @@ class Command(BaseCommand):
                     path_name = path_info.strip("/").replace('/', '.') or "root"
                     profname = "%s.%s.prof" % (path_name, datetime.now().isoformat())
                     profname = os.path.join(prof_path, profname)
-                    if USE_CPROFILE:
-                        prof = cProfile.Profile()
-                    else:
-                        prof = hotshot.Profile(profname)
+                    prof = hotshot.Profile(profname)
                     start = datetime.now()
                     try:
                         return prof.runcall(inner_handler, environ, start_response)
@@ -177,21 +108,15 @@ class Command(BaseCommand):
                         # seeing how long the request took is important!
                         elap = datetime.now() - start
                         elapms = elap.seconds * 1000.0 + elap.microseconds / 1000.0
+                        prof.close()
                         profname2 = "%s_%06dms_%s.prof" % (path_name, elapms, datetime.now().isoformat())
                         profname2 = os.path.join(prof_path, profname2)
-                        if USE_LSPROF:
-                            if os.path.exists('/usr/bin/'):
-                                prof.dump_stats(profname)
-                                assert os.spawnlp(
-                                    os.P_WAIT, "hotshot2calltree", "hotshot2calltree",
-                                    profname, profname2) == 0
-                            else:
-                                kg = KCacheGrind(prof)
-                                kg.output(file(profname2, 'w'))
-                        elif USE_CPROFILE:
-                            prof.dump_stats(profname2)
-                        else:
-                            os.rename(profname, profname2)
+                        def lawl():
+                            time.sleep(0.2)
+                            os.spawnlp(os.P_WAIT, "hotshot2calltree", "hotshot2calltree",
+                                       profname, '-o', profname2)
+                            os.unlink(profname)
+                        thread.start_new_thread(lawl, ())
                 return handler
 
             print "Validating models..."
